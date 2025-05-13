5a07ceb6b81baaf90b459790736833ea014ed8e4
Rikardo12-my <jetlimanik@gmail.com>
2025-05-13 21:52:36 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,9 +1,9 @@
 <?php
-session_start(); // Pastikan session dimulai di sini
+session_start(); 
 
 include('koneksi.php');
 
-// Mengecek apakah pengguna sudah login
+
 
 
 $x = $_GET['nim'];
@@ -11,7 +11,7 @@ $sql = "DELETE FROM data_mahasiswa WHERE nim = '$x'";
 $exe = $conn->query($sql);
 
 if ($exe) {
-    // Redirect ke halaman daftar dosen setelah penghapusan berhasil
+   
     header("Location:index.php?page=data_mahasiswa");
 } else {
     echo "Gagal menghapus data.";
